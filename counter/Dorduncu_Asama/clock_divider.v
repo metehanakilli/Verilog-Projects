5ec81a650d0c371ee393a24b549f09dca945353c
metehanakilli <115922752+metehanakilli@users.noreply.github.com>
2026-07-06 15:36:39 +0300

    Refactor clock_divider module parameters and logic
    Refactor clock_divider module with improved parameter naming and reset logic.

--- counter/Dorduncu_Asama/clock_divider.v
+++ counter/Dorduncu_Asama/clock_divider.v
@@ -1,36 +1,33 @@
 /*
- * Dosya Adi       : clock_divider.v
- * Proje Adi       : CLOCK DIVIDER 
- * Yazar           : [METEHAN AKILLI / Sirket Adi]
- * Tarih           : [03/07/2026]
+ * File Name       : clock_divider.v
+ * Project Name    : Clock Divider
+ * Author          : [METEHAN AKILLI / Sirket Adi]
+ * Date            : [03/07/2026]
  * 
- * Açiklama        : 
-
- * 
- * Revizyon Gecmisi:
- * Tarih           Yazar              Aciklama
- * ----------      ---------------    -----------------------------------------
- * 03.07.2026      [METEHAN]           Ilk surum olusturuldu.
+ * Comment     : 
 */
 
-module clock_divider #( parameter divide_rate = 2
-)( 
-	input wire clk, rst,
-	output reg clk_out
+module clock_divider 
+#( parameter DIVIDE_RATE = 2,			//division rate of system clock
+   parameter N_BIT = 32						//BIT RATE OF COUNTER REG 
+   
+)(  input wire clk,				//System Clock : 100MHz / Input clock of Divider
+	input wire rst,				//System reset
+	output reg clk_out			//Divided Clock / Output Clock of Divider
 );
 	
-	reg [31 : 0] counter;
+	reg [$clog2(DIVIDE_RATE) : 0] counter;		  //Counter reg for divide system clock, triggers clk_out
 	
-	always @(posedge clk) begin
-		if (rst == 1) begin
-			counter <= 32'd0;
-			clk_out <= 1'b0;
+	always @(posedge clk or posedge rst) begin
+		if (rst) begin
+			counter <= 0;				//If rst triggered --> reset the counter and clk_out 
+			clk_out <= 0;
 		end else begin
-			if (counter >= (divide_rate / 2) - 1) begin
-				counter <= 32'd0;
-				clk_out <= ~clk_out;
+			if (counter >= (DIVIDE_RATE >> 1) - 1) begin
+				counter <= 0;
+				clk_out <= ~clk_out;				
 			end else begin
-				counter <= counter + 32'd1;
+				counter <= counter + 1;
 			end
 		end
 	end
